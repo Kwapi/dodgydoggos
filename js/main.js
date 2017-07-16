48a5115fde508fd2b8b3fcf478fd6caeff9d474d
Michal <m.zak08@gmail.com>
2017-07-16 23:10:18 +0100

    bum 4

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $('img').click(function () {
     alert('bork bork');
 });
+
+
+console.log('%c       ', 'font-size: 200px; background: url(https://media.tenor.com/images/da40fc8893eabfdb611d6677388a3472/tenor.gif) no-repeat;');
