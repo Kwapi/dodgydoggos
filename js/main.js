9060f61cdceefbd3d8d8d4d20a4ff139eaf5b678
Michal <m.zak08@gmail.com>
2017-07-23 16:52:00 +0100

    borked tracking

--- js/main.js
+++ js/main.js
@@ -3,4 +3,12 @@ $('img').click(function () {
 });
 
 
+// track everything / control everyone
+ga('send', {
+    hitType: 'click',
+    eventCategory: 'Doggo',
+    eventAction: 'borked',
+});
+
+// hack away doggo
 console.log('%c       ', 'font-size: 200px; background: url(https://media.tenor.com/images/da40fc8893eabfdb611d6677388a3472/tenor.gif) no-repeat;');
